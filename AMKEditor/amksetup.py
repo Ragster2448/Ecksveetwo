775987524a03443715fbde67d11478670bd92fa7
Ragster2448 <72439059+Ragster2448@users.noreply.github.com>
2020-10-08 14:44:31 -0400

    Update amksetup.py

--- AMKEditor/amksetup.py
+++ AMKEditor/amksetup.py
@@ -4,7 +4,7 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
 
 setup(
-    options = {'py2exe': {'includes': ['tkinter'], 'bundle_files': 2, 'compressed': True}},
+    options = {'py2exe': {'bundle_files': 2, 'compressed': True}},
     windows = [{'script': 'amkeditor.pyw', 'icon_resources': [(1, 'amkeditor.ico')]}],
     zipfile = None
 )
